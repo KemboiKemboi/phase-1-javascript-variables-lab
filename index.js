c4556135beaf71a5db1c179a0768b69b44437404
Felix Kemboi <felixkemboi824@gmail.com>
2025-03-24 11:18:09 +0300

    initiate commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution in this file!
+// Declare companyName as a const
+const companyName = 'Scuber';
+
+// Declare mostProfitableNeighborhood using let
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Declare companyCeo using let
+let companyCeo = 'Susan Smith';
+
